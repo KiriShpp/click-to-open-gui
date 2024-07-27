9cfad88f0834b732124a53ccee0dafa847599248
Kiri <kirishpp@gmail.com>
2024-07-27 15:23:21 +0300

    opengui.lua

---
+++ opengui.lua
@@ -0,0 +1,3 @@
+game.Players.LocalPlayer.PlayerGui.buttons.button.MouseButton1Click:Connect(function() -- Replace "buttons" with the name of your directory with buttons :: Replace "button" with the name of your button
+    game.Players.LocalPlayer.PlayerGui.GUI.Enabled = true -- Replace "GUI" with the name of your GUI Menu
+end)
